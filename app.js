b8d7cfeb0145de3eb58ab67a24b6689e4dcbca46
Volodymyr Kryvutsa <volodymyrkr@softwareplanetgroup.com>
2025-05-06 14:12:08 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 8080;
+
+http.createServer((req, res) => {
+    res.end("Hello from TSD Buildpack test!");
+}).listen(PORT, () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}/`);
+});
