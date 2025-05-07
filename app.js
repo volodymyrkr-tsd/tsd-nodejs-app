1114752900d158964cfdd8f91030fce2d706c86c
Volodymyr Kryvutsa <volodymyrkr@softwareplanetgroup.com>
2025-05-07 11:50:12 +0300

    fix app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
-    res.end("Hello from TSD Buildpack test!");
+    res.end("Hello from TSD Buildpack test2!");
 }).listen(PORT, () => {
     console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
